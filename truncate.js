bc6ef7498fe0fe38fba79f0c8fd0d273a2a25ad2
acatella <aficclark@gmail.com>
2016-01-09 17:31:06 -0800

    wrote script, still need to do stretch goals

--- truncate.js
+++ truncate.js
@@ -1,23 +1,51 @@
-// This is the file where you will write the Truncate Words function and related code.
 
-// TODO: Create a Function called truncateWords() that accepts two arguments: 
-// longText (a String with several words in it)
-// numWords (an Integer that sets the number of words you want in the returned text)
+// TODO:
+// create an html page to display the object
+// strech goal:
+    // Place "use strict" at the top of your file to invoke strict mode
+    // Create an additional function called truncateCharacters and repeat this experiment truncating by characters instead of words
+    // Modify your Function so that it can be called without the numWords argument (if no numWords is supplied it should use a default number of words to truncate)
+    // Modify your Function so that instead of returning a single String value it returns an Object that contains the following information:
+    //     originalText (original text that was sent to the function)
+    //     wordCount (count of the number of words in original text)
+    //     numWords (the original number that was sent in)
+    //     shortText (truncated String)
+    //     (Don't forget to modify your console.log statements to properly show all this info about the object.)
 
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
-
-// This portion of the script is meant to call and display the result of your Function.
-// You do not need to change the following lines, but you may change them if you 
-// are experimenting further or pursuing stretch goals.
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
-var shortText = truncateWords(originalText, wordLimit);
-console.log('originalText: ' + originalText);
-console.log('shortText: ' + shortText);
+
+//function call
+truncateWords(originalText,wordLimit);
+
+
+//object constructor
+function sentence(original,wordCount,limit,shortened) {
+  this.originalText = original;
+  this.wordCount = wordCount;
+  this.numWords = limit;
+  this.shortText = shortened;
+}
+
+function truncateWords(str, limit) {
+  //build arrays and word count
+  var words = str.split(" ");
+  var wordCount = words.length;
+  var diff = wordCount - limit;
+
+  if (diff > 0) {
+    words.splice(limit,diff, "...");
+    var shortened = words;
+  }
+
+  else {var shortened = words};
+
+  //put array back into string
+  shortened = shortened.join(" ");
+
+  //build object to return
+  var strObj = new sentence(str,wordCount,limit,shortened);
+  for (prop in strObj) {
+    document.write(prop + ": " + strObj[prop] + "<br>");
+  }
+}
