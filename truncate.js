25939c610c1706498eb2dfd311fe51d8592e5404
acatella <aficclark@gmail.com>
2016-01-14 19:17:33 -0800

    did stretch goals

--- truncate.js
+++ truncate.js
@@ -1,22 +1,9 @@
 
-// TODO:
-// create an html page to display the object
-// strech goal:
-    // Place "use strict" at the top of your file to invoke strict mode
-    // Create an additional function called truncateCharacters and repeat this experiment truncating by characters instead of words
-    // Modify your Function so that it can be called without the numWords argument (if no numWords is supplied it should use a default number of words to truncate)
-    // Modify your Function so that instead of returning a single String value it returns an Object that contains the following information:
-    //     originalText (original text that was sent to the function)
-    //     wordCount (count of the number of words in original text)
-    //     numWords (the original number that was sent in)
-    //     shortText (truncated String)
-    //     (Don't forget to modify your console.log statements to properly show all this info about the object.)
-
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
 
 //function call
-truncateWords(originalText,wordLimit);
+truncateWords(originalText, wordLimit);
 
 
 //object constructor
@@ -44,15 +31,36 @@ function truncateWords(str, limit) {
     var shortened = words;
   }
 
-  else {var shortened = words};
+  else {
+    var shortened = words;
+    //give limit value equal to string for output to object later
+    limit = shortened.length;
+  }
 
   //put array back into string
   shortened = shortened.join(" ");
 
   //build object to return
-  var objName = 'sentence_' + wordLimit;
+  var objName = 'truncateTo_' + wordLimit;
   var strObj = new sentence(objName,str,wordCount,limit,shortened);
   for (prop in strObj) {
     document.write(prop + ": " + strObj[prop] + "<br>");
   }
 }
+
+//this function will only return the truncated string
+function truncateCharacters(str, limit) {
+  if (!limit || limit == undefined) {
+    limit = 25;
+  }
+
+  var chars = str.split("");
+  var difference = chars.length - limit;
+  chars.splice(limit,difference,"...");
+  var shortenedStr = chars.join("");
+
+  document.write("<br>" + shortenedStr);
+}
+
+//second function call
+truncateCharacters(originalText,55);
